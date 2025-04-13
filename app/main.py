8c3d0898c8d737ac40b4898d15f49551e4879b7b
UjuAyoku <63997970+UjuAyoku@users.noreply.github.com>
2025-04-13 17:25:10 -0600

    Added the pysqlite3 import required to deploy app on Streamlit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import sys
+import pysqlite3
+sys.modules["sqlite3"] = pysqlite3
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
@@ -10,7 +13,7 @@ def create_streamlit_app(llm, portfolio, clean_text):
     # st.title("📧 Cold Mail Generator")
     st.title(":e-mail: Email Generator")
     # specify an ACTIVE job posting. Using an expired job link will not work.
-    url_input = st.text_input("Enter a URL:", value="https://jobs.bmo.com/ca/en/job/R250008982/Vice-President-Global-Markets-Engineering")
+    url_input = st.text_input("Enter a URL:", value="https://careers.arcresources.com/job/Calgary-Senior-Data-Scientist-Albe/589984817/")
     submit_button = st.button("Submit")
 
     if submit_button:
